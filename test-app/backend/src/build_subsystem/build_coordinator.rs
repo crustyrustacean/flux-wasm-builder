78e7448ea977279869016d4168767c6121600a8e
Jeff Mitchell <crusty.rustacean@gmail.com>
2026-03-01 19:57:26 -0800

    chore: delete mistaken addition of test-app

--- test-app/backend/src/build_subsystem/build_coordinator.rs
+++
@@ -1,1 +0,0 @@
-// Implementation will be added in a future stage
--- test-app/backend/src/build_subsystem/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod build;
-pub mod build_coordinator;
-pub mod watcher;
-pub mod reload;
-pub mod static_assets;
-
-pub use build::{BuildConfig, BuildError, run_wasm_pack};
-pub use static_assets::{serve_pkg_file, spa_fallback};
